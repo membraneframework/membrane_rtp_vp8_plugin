4f862a4f255ce36b4b0a1c2f5a97b827508f099d
Mateusz Front <mateusz.front@swmansion.com>
2022-12-19 13:07:37 +0100

    bump version to 0.7

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.VP8.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.6.1"
+  @version "0.7.0"
   @github_url "https://github.com/membraneframework/membrane_rtp_vp8_plugin"
 
   def project do
