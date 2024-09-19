cd6e63171bcc5727c65f3336f58d534ea2bcc9c1
Łukasz Kita <lukasz.kita0@gmail.com>
2024-09-19 10:38:47 +0200

    Set version to v0.9.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.VP8.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.10.0"
+  @version "0.9.2"
   @github_url "https://github.com/membraneframework/membrane_rtp_vp8_plugin"
 
   def project do
