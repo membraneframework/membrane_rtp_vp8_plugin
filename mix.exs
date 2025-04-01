7ab3bb604e1c3daed7a6c4f3c580caea0b1b884d
noarkhh <jakub.pryc@gmail.com>
2025-04-01 19:21:39 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.VP8.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.9.4"
+  @version "0.9.5"
   @github_url "https://github.com/membraneframework/membrane_rtp_vp8_plugin"
 
   def project do
